10814633bb9510a7c03670cc19816887771f008c
Adam Beddoe <ab16301@my.bristol.ac.uk>
2017-11-27 16:13:47 +0000

    added config file

---
+++ src/config.h
@@ -0,0 +1,6 @@
+#define  IMHT 16                  //image height
+#define  IMWD 16                 //image width
+#define  noOfThreads 2			  //Our implementation requires that this must be 2^n
+#define  debugMode 0
+
+
