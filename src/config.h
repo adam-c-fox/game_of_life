6b15f56082f0d97703291e82493e8a21f5583965
adam-c-fox <af16371@my.bristol.ac.uk>
2017-11-28 22:26:29 +0000

    added report and generated images

--- src/config.h
+++ src/config.h
@@ -1,13 +1,13 @@
-#define  IMHT 16                  //image height
-#define  IMWD 16                //image width
-#define  INPUT "test.pgm"
+#define  IMHT 1344                  //image height
+#define  IMWD 1344                //image width
+#define  INPUT "64x64.pgm"
 #define  OUTPUT "testout"
 #if IMHT%8 != 0
   #error "WHAT ARE YOU DOING?"
 #endif
-#define  noOfThreads 2			  //Our implementation requires that this must be 2^n
-#define  debugMode 1
-#define  generateOnBoard 0		//If true, will ignore input name
-#define  printAllImages 0
+#define  noOfThreads 8			  //Our implementation requires that this must be 2^n
+#define  debugMode 0
+#define  generateOnBoard 1		//If true, will ignore input name
+#define  printAllImages 1
 
 
--- src/main.xc
+++ src/main.xc
@@ -365,12 +365,12 @@ int main(void) {
         on tile[1]: DataOutStream(OUTPUT, c_outIO);   
         on tile[1]: distributor(c_inIO, c_outIO, c_control, dist, c_buttons, c_leds);
 
-        on tile[0]: if (debugMode) test();
+        //on tile[0]: if (debugMode) test();
 
         on tile[0]: buttonListener(buttons, c_buttons);
         on tile[0]: showLEDs(leds, c_leds);
     
-        on tile[1]: par (int i = 0; i < (noOfThreads/2); i++) {
+        on tile[0]: par (int i = 0; i < (noOfThreads/2); i++) {
             packedChunkWorker(i, dist[i], worker[i], worker[(i+1)%noOfThreads]);
         }
 
